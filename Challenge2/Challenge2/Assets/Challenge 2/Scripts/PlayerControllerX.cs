74b8747a6a33b1466833cc4b0c1cd72e505ebbac
JetLee <asanofelipe@gmail.com>
2023-08-15 10:11:48 -0300

    chalenger2 conclued

---
+++ Challenge2/Challenge2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Timers;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    private float gunHeat;
+    private const float TimeDelay = 3.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gunHeat > 0)
+        {
+            gunHeat -= Time.deltaTime;
+            Debug.Log("Delay Spawn!");
+        }
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(gunHeat <= 0)
+            {
+                gunHeat = TimeDelay;
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            }
+        }
+    }
+
+    void MyFunction()
+    {
+        Debug.Log("Delay Spawn!");
+    }
+}
