1ab04a58fa9633d16a4a5a6eedb76423d32d4bbd
JetLee <asanofelipe@gmail.com>
2023-08-18 11:13:21 -0300

    ModTheCube conclued

---
+++ ModTheCube/Assets/ModTheCube/Cube.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public MeshRenderer Renderer;
+    
+    void Start()
+    {
+        transform.position = new Vector3(0, 1, 0);
+        transform.localScale = Vector3.one * 2.0f;
+        
+        Material material = Renderer.material;
+        
+        material.color = new Color(0.5f, 1.0f, 0.3f, 0.4f);
+    }
+    
+    void Update()
+    {
+        transform.Rotate(50.0f * Time.deltaTime, 0.0f, 0.0f);
+    }
+}
---
+++ ModTheCube/Readme.md
@@ -0,0 +1,9 @@
+# Atividade WebGL
+
+Atividade desenvolvendo aprendizado em WebGL
+
+Compartilhando o trabalho de modo Web
+
+1. Pasta "ModTheCubeChallenge_Package" contendo Package do projeto
+1. Pasta "Build" contendo o Build do projeto, disponivel para executar o mesmo
+1. Link para acesso do projeto compartilhado: [WebGL_Link](https://play.unity.com/mg/other/build-ewk)
