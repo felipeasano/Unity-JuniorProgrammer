b8e72b383724e8bd8e9312b64782cf6faec57a92
Felipe <asanofelipe@gmail.com>
2023-08-22 10:17:31 -0300

    conclued lesson 3.1

---
+++ Unity_3/Prototype3/Prototype3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody playerRB;
+    public float jumpForce = 10f;
+    public float gravityModifier;
+    public bool isOnGround = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround=true;
+    }
+}
---
+++ Unity_3/Prototype3/Prototype3/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+    private Vector3 spawnPos = new Vector3(35,0,0);
+
+    private float startDelay = 2f;
+    private float repeatRate = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void SpawnObstacle()
+    {
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+
+    }
+}
