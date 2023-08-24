cb39a35c9f7032933ab05bf8426c6f9d6faf22d4
JetLee <asanofelipe@gmail.com>
2023-08-24 17:28:35 -0300

    lesson 3.3 conclued

--- Unity_3/Prototype3/Prototype3/Assets/Scripts/PlayerController.cs
+++ Unity_3/Prototype3/Prototype3/Assets/Scripts/PlayerController.cs
@@ -10,20 +10,24 @@ public class PlayerController : MonoBehaviour
     public float gravityModifier;
     public bool isOnGround = true;
     public bool gameOver;
+
+    private Animator playerAnim;
     // Start is called before the first frame update
     void Start()
     {
         playerRB = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
         }
     }
 
@@ -38,6 +42,8 @@ public class PlayerController : MonoBehaviour
         {
             Debug.Log("Game Over!");
             gameOver = true;
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
         }
     }
 }
