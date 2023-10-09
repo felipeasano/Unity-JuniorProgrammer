08d4b0cbc4f59c169eaeed337ed096746ce9b16f
Felipe <asanofelipe@gmail.com>
2023-10-09 07:32:14 -0300

    Bonus 3 coclued

--- Unity_3/Prototype3_3/Assets/Scripts/GameManager.cs
+++ Unity_3/Prototype3_3/Assets/Scripts/GameManager.cs
@@ -8,11 +8,42 @@ public class GameManager : MonoBehaviour
     public float score;
     private PlayerController playerControllerScript;
 
+    // para realizar a animaçao de start
+    public Transform startingPoint;
+    public float lerpSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
         playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
         score = 0;
+
+        playerControllerScript.gameOver = true;
+        StartCoroutine(PlayIntro());
+    }
+
+    IEnumerator PlayIntro()
+    {
+        Vector3 startPos = playerControllerScript.transform.position;
+        Vector3 endPos = startingPoint.position;
+        float journeyLength = Vector3.Distance(startPos, endPos);
+        float startTime = Time.time;
+
+        float distanceCovered = (Time.time - startTime) * lerpSpeed;
+        float fractionOfJourney = distanceCovered / journeyLength;
+
+        playerControllerScript.GetComponent<Animator>().SetFloat("Speed_Multiplier", 0.5f);
+
+        while(fractionOfJourney < 1)
+        {
+            distanceCovered = (Time.time - startTime) * lerpSpeed;
+            fractionOfJourney = distanceCovered / journeyLength;
+            playerControllerScript.transform.position = Vector3.Lerp(startPos, endPos, fractionOfJourney);
+            yield return null;
+        }
+
+        playerControllerScript.GetComponent<Animator>().SetFloat("Speed_Multiplier", 1.0f);
+        playerControllerScript.gameOver = false;
     }
 
     // Update is called once per frame
