8bd3ec3d30862c6f1db4d84e199c0f54c1aedb5a
Felipe <asanofelipe@gmail.com>
2023-12-13 08:03:13 -0300

    wip unity4

---
+++ Unity_4/Prototype4/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+
+    private float spawnRange = 9.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateSpawnPosition();
+
+        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private Vector3 GenerateSpawnPosition()
+    {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+
+        return new Vector3(spawnPosX, 0, spawnPosZ);
+    }
+}
